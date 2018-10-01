4029cf71271b8931a75b9c5c18099d8818049035
EstevaoST <estevao.testa@acad.pucrs.br>
2018-10-01 14:44:49 -0300

    Tudo

---
+++ Assets/PlayerPaddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddle : MonoBehaviour {
+
+    public Rigidbody myRigidbody;
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xinput = Input.GetAxis("Horizontal");
+
+        myRigidbody.velocity = transform.right * xinput * speed;
+
+	}
+}
